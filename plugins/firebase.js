565c151bc890a96ce42f64145bb76d9846b07301
Jan Woll <144726069+Trawollta@users.noreply.github.com>
2024-10-11 08:57:08 +0200

    changes in structure

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCdiYq1m0ryEyDgEIAGssuEhokhkn0xCpw",
-  authDomain: "nuxt-join-228b9.firebaseapp.com",
-  projectId: "nuxt-join-228b9",
-  storageBucket: "nuxt-join-228b9.appspot.com",
-  messagingSenderId: "668353619728",
-  appId: "1:668353619728:web:57ab992d51c246cf152795"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
